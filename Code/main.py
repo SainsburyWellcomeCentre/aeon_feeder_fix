0efb4a2376a11360bbcd2d1be2a804161e826fca
glopesdev <glopesdev@users.noreply.github.com>
2023-12-20 17:37:55 +0000

    Hold line HIGH for X consecutive inactive readings

--- Code/main.py
+++ Code/main.py
@@ -14,16 +14,21 @@ led = Pin(14, Pin.OUT)
 #tim.init(freq=1000, mode=Timer.PERIODIC, callback=tick)
 count = 0
 conversion_factor = 3.3 / (65535)
+ready = 0
 
 while True:
     led.value(1)
     reading = sensor.read_u16()# * conversion_factor
     led.value(0)
     if reading < 30000:
+        ready = 3
         out.value(1)
         count=count+1
+    elif ready > 1:
+        ready = ready - 1
     else:
         out.value(0)
+        ready = 0
 #    print(reading)
     print(count)
     utime.sleep(0.003)
